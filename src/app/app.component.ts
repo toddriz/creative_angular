a1d50c5fdf6676e7f95dfeba74cc8ba2dd106afd
Todd Risenmay <toddriz@gmail.com>
2017-10-18 16:54:59 -0600

    added app main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RockInfoComponent } from './rock-info/rock-info.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
     selector: 'app-root',
